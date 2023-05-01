7602923e85ac2cea4b2f6a35ccb4c299f38142fc
savkabober <savmalyshev@mail.ru>
2023-05-01 21:35:21 +0300

    добавил алгоритм распознвания удара(вперед/вправо/влево)

--- firmware/Tests/OpenMV.py
+++ firmware/Tests/OpenMV.py
@@ -6,41 +6,51 @@ uart = UART(1, 9600)
 sensor.reset()                      # Reset and initialize the sensor.
 sensor.set_pixformat(sensor.RGB565) # Set pixel format to RGB565 (or GRAYSCALE)
 sensor.set_framesize(sensor.QQVGA)   # Set frame size to QVGA (320x240)
-sensor.skip_frames(time = 2000)     # Wait for settings take effect.
+#sensor.skip_frames(time = 2000)     # Wait for settings take effect.
 clock = time.clock()                # Create a clock object to track the FPS.
 
-sensor.set_auto_exposure(False)
-current_exposure_time_in_microseconds=  sensor.get_exposure_us()
-sensor.set_auto_exposure(False, \
-    exposure_us = int(current_exposure_time_in_microseconds* 1.4))
+#current_exposure_time_in_microseconds=  sensor.get_exposure_us()
+#sensor.set_auto_exposure(False, \
+    #exposure_us = 100000)
+sensor.set_contrast(3)
+sensor.set_saturation(3)
+sensor.set_brightness(3)
+sensor.set_quality(100)
+sensor.set_auto_exposure(False, exposure_us=10000)
+#print(current_exposure_time_in_microseconds)
 
 keye = 0.25
 
-gals = (0, 100, -16, 15, 37, 68)
+gals = (43, 100, 20, 125, 36, 127)
 
-ruka = (10, 43, -15, 16, -128, -16)
+ruka = (25, 93, -42, -17, -4, 112)
 
 while(True):
     #clock.tick()                    # Update the FPS clock.
     img = sensor.snapshot()
+    img.lens_corr(1.3)
     gals_pix = [0, 0, 0, 0]
     ruka_pix = [0, 0, 0, 0]
-    for g in img.find_blobs([gals], pixels_threshold=100):
+    for g in img.find_blobs([gals], pixels_threshold=100, roi=(0, img.height() // 2, img.width(), img.height() // 2)):
         if g.pixels() > ruka_pix[0]:
             gals_pix[0] = g.pixels()
             gals_pix[1] = g.x() + g.w() // 2 - img.width() // 2
             gals_pix[2] = g.w()
             gals_pix[3] = g.rect()
-    for r in img.find_blobs([ruka], pixels_threshold=100):
+    for r in img.find_blobs([ruka], pixels_threshold=100, roi=(0, 0, img.width(), img.height() * 3 // 4)):
         if r.pixels() > ruka_pix[0]:
             ruka_pix[0] = r.pixels()
             ruka_pix[1] = r.x() + r.w() // 2 - img.width() // 2
-            ruka_pix[2] = r.w()
+            ruka_pix[2] = r.h() / r.w()
             ruka_pix[3] = r.rect()
+            if gals_pix[1] != 0 and gals_pix[2] != 0 and ruka_pix[1] != 0:
+                ruka_pix[1] = abs(ruka_pix[1] - gals_pix[1]) / gals_pix[2]
+            else:
+                ruka_pix[1] = 0
     tag0 = 0
     tag_rect = 0
     for tag in img.find_apriltags(families=1):
-        if tag.id() == 0:
+        if tag.id() == 3:
             tag0 = 1
             tag_rect = tag.rect()
     if gals_pix[3] != 0:
@@ -49,17 +59,24 @@ while(True):
         img.draw_rectangle(ruka_pix[3])
     if tag_rect != 0:
         img.draw_rectangle(tag_rect)
-    if gals_pix[1] != 0 and gals_pix[2] != 0 and ruka_pix[1] != 0:
-        delta_gr = abs(ruka_pix[1] - gals_pix[1]) / gals_pix[2]
-    else:
-        delta_gr = 0
-    toUart = tag0
-    toUart += min(gals_pix[1], 99) * 2
-    toUart += min(gals_pix[2], 99) * 200
-    toUart += min(ruka_pix[2], 99) * 20000
-    toUart += int(delta_gr * 100) * 2000000
-    print('toUart', toUart)
-    uart.write(str(int(toUart)))
-    uart.write("_")
+    udar = 0
+    if ruka_pix[1] != 0 and ruka_pix[2] != 0:
+        if ruka_pix[2] > 1.7:
+            udar = 2
+        else:
+            if ruka_pix[1] > 4.5:
+                udar = 1
+            else:
+                udar = 3
+    #print(ruka_pix[1], ruka_pix[2])
+    print(udar)
+    #toUart = tag0
+    #toUart += min(gals_pix[1], 99) * 2
+    #toUart += min(gals_pix[2], 99) * 200
+    #toUart += min(ruka_pix[2], 99) * 20000
+    #toUart += int(delta_gr * 100) * 2000000
+    ##print('toUart', toUart)
+    #uart.write(str(int(toUart)))
+    #uart.write("_")
     #print(clock.fps())              # Note: OpenMV Cam runs about half as fast when connected
                                     # to the IDE. The FPS should increase once disconnected.
